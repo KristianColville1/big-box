a310bd8136d1ef08ca6e474a9d355e852f0948d4
Kristian Philip Colville <kristiancolville1@gmail.com>
2021-12-09 16:23:21 +0000

    file uploads- nothing special yet

---
+++ small_box/back-end stuff/basic.py
@@ -0,0 +1,24 @@
+# Why is python so Great?
+# it is object-orientated and I know my around an object..
+# sounds weird but I ment because I already know alot of object-orientated design so should be easy..
+# fill this basic fill up with cheat syntax over the next few days...
+
+a = 6
+b = 9
+def maple():
+    if a > b:
+        print('yes the larger number is larger')
+    else:
+        print('impossible..')
+        print('idiot!, check your comparison operator..')
+
+maple()
+
+# reasons to dislike python(sarcasim):
+# 1 it is too easy to write
+# 2 i have to guess my data types now :(
+# 3 it will save me unnecessary time writing code
+
+
+
+# check for spell checker on vs code - will need it with the way i type
