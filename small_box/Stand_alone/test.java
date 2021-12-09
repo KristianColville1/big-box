cf9c65e09cdd48928b2fec5450fac94f31c08c46
Kristian Philip Colville <kristiancolville1@gmail.com>
2021-12-09 15:58:03 +0000

    changed file structure for compatibility reasons between front and back end folders

---
+++ small_box/Stand_alone/test.java
@@ -0,0 +1,13 @@
+package small_box.Stand_alone;
+
+// testing installation of java on this IDE 
+// will be back soon to create files and experiment for exam.
+public class test {
+    public static void main(String[] args) {
+        int a = 484;
+        int aa = 94;
+        int ab = a + aa;
+
+        System.out.println(ab);
+    }
+}
